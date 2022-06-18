eddc3c02666e12e24efe479b70e997cdc0709b56
Cyrill Bolliger <mail@cyrill.me>
2022-06-18 22:24:26 +0200

    [FEAT] BaseNotice component

--- app/src/components/base/BaseButtonClose.vue
+++ app/src/components/base/BaseButtonClose.vue
@@ -7,6 +7,11 @@
         viewBox="0 0 30 30"
         preserveAspectRatio="xMinYMin meet"
         class="collectme-base-button-close__svg"
+        :class="{
+        'collectme-base-button-close__svg--primary': !secondary,
+        'collectme-base-button-close__svg--secondary': secondary,
+        'collectme-base-button-close__svg--muted': muted,
+      }"
     >
       <g>
         <path stroke-width="2" fill="none" d="M29 1L1 29M1 1l28 28"/>
@@ -16,7 +21,16 @@
 </template>
 
 <script setup lang="ts">
-
+const props = defineProps({
+  secondary: {
+    type: Boolean,
+    default: false,
+  },
+  muted: {
+    type: Boolean,
+    default: false,
+  }
+})
 </script>
 
 <style>
@@ -25,15 +39,36 @@
 }
 
 .collectme-base-button-close__svg {
-  stroke: var(--color-grey-2);
   width: 100%;
   height: 100%;
   transition-duration: var(--transition-speed-fast);
   transition-timing-function: ease-out;
 }
 
-.collectme-base-button-close__svg:hover,
-.collectme-base-button-close__svg:focus {
+.collectme-base-button-close__svg--primary {
   stroke: var(--color-primary);
+}
+
+.collectme-base-button-close__svg--primary:hover,
+.collectme-base-button-close__svg--primary:focus {
+  stroke: var(--color-primary-dark);
+}
+
+.collectme-base-button-close__svg--secondary {
+  stroke: var(--color-secondary);
+}
+
+.collectme-base-button-close__svg--secondary:hover,
+.collectme-base-button-close__svg--secondary:focus {
+  stroke: var(--color-secondary-dark);
+}
+
+.collectme-base-button-close__svg--muted {
+  stroke: var(--color-grey-2);
+}
+
+.collectme-base-button-close__svg--muted:hover,
+.collectme-base-button-close__svg--muted:focus {
+  stroke: var(--color-grey-3);
 }
 </style>
---
+++ app/src/components/base/BaseNotice.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="collectme-base-notice">
+    <slot></slot>
+    <BaseButtonClose
+        v-if="props.dismissible"
+        @click="$emit('dismiss')"
+        class="collectme-base-notice__dismiss"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseButtonClose from '@/components/base/BaseButtonClose.vue'
+
+defineEmits<{
+  (e: 'dismiss'): void
+}>()
+
+
+const props = defineProps({
+  dismissible: {
+    type: Boolean,
+    required: false,
+    default: true
+  },
+})
+
+</script>
+
+<style>
+.collectme-base-notice {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  gap: clamp(10px, 10px + 10 * (100vw - 375px) / 1305, 20px);
+  background-color: var(--color-primary-super-light);
+  padding: clamp(10px, 10px + 10 * (100vw - 375px) / 1305, 20px);
+  border-radius: 5px;
+  color: var(--color-primary-dark);
+}
+
+.collectme-base-notice__dismiss {
+  width: 20px;
+  stroke: var(--color-primary-dark);
+  flex: none;
+}
+</style>
