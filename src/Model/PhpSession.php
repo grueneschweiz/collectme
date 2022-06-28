d07c7c082233dece667c84aa691bb21a233caaf2
Cyrill Bolliger <mail@cyrill.me>
2022-06-28 11:37:46 +0200

    [FEAT] reset session

--- src/Model/PhpSession.php
+++ src/Model/PhpSession.php
@@ -27,4 +27,23 @@ class PhpSession
 
         $_SESSION[AUTH_SESSION_KEY] = $persistentSession;
     }
+
+    public function reset(): void
+    {
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 1,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+
+        unset($_SESSION[AUTH_SESSION_KEY]);
+        session_regenerate_id(true);
+    }
 }
