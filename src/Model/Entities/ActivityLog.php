b8a1d3c3b3ebd0255d293b57cc07c54f98e6c934
Cyrill Bolliger <mail@cyrill.me>
2022-07-01 09:11:00 +0200

    [FEAT] find activity logs by cause

--- src/Model/Entities/ActivityLog.php
+++ src/Model/Entities/ActivityLog.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Collectme\Model\Entities;
 
+use Collectme\Exceptions\CollectmeDBException;
 use Collectme\Model\Database\DBField;
 use Collectme\Model\Database\DBTable;
 use Collectme\Model\Entity;
+use Collectme\Model\Filter;
 use Collectme\Model\JsonApi\ApiModelAttribute;
 use Collectme\Model\JsonApi\ApiModelRelationship;
 use Collectme\Model\JsonApi\ApiModelType;
+use Collectme\Model\Paginator;
 
 #[DBTable('activity_logs')]
 #[ApiModelType('activity')]
@@ -39,6 +42,44 @@ class ActivityLog extends Entity
         ?\DateTime $deleted = null
     ) {
         parent::__construct($uuid, $created, $updated, $deleted);
+    }
+
+    /**
+     * @return ActivityLog[]
+     * @throws CollectmeDBException
+     */
+    public static function findByCause(
+        string $causeUuid,
+        ?Paginator $pagination = null,
+        ?Filter $filter = null
+    ): array {
+        global $wpdb;
+
+        $table = self::getTableName();
+
+        $rawQuery = "SELECT * FROM {$table} WHERE causes_uuid = '%s' AND deleted_at IS NULL";
+
+        $args = [$causeUuid];
+
+        if ($filter) {
+            $rawQuery = $filter->addToQuery($rawQuery, $args);
+        }
+
+        if ($pagination) {
+            $rawQuery = $pagination->addToQuery(
+                $rawQuery,
+                $args,
+                'insert_id',
+                self::getTableName()
+            );
+        }
+
+        $query = $wpdb->prepare(
+            $rawQuery,
+            ...$args
+        );
+
+        return self::findByQuery($query);
     }
 
     protected static function _convertFromType(string $type): EnumActivityType
