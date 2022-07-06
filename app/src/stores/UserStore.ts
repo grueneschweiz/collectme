b8e517c6cccabab978d54899add9ac5eb1721d5a
Cyrill Bolliger <mail@cyrill.me>
2022-07-06 18:09:42 +0200

    [FEAT] user store

---
+++ app/src/stores/UserStore.ts
@@ -0,0 +1,39 @@
+import {defineStore} from 'pinia';
+import api from '@/utility/api';
+import type {AxiosError, AxiosResponse} from "axios";
+import type {User} from "@/models/generated";
+
+const endpointUrl = `user/me`;
+
+interface UserResponseSuccess extends AxiosResponse {
+    data: {
+        data: User;
+    }
+}
+
+interface UserStoreState {
+    me: User|null;
+    isLoading: boolean;
+    error: AxiosError | null;
+}
+
+export const useUserStore = defineStore('UserStore', {
+    state: (): UserStoreState => {
+        return {
+            me: null,
+            isLoading: false,
+            error: null,
+        }
+    },
+
+    actions: {
+        async fetch() {
+            this.isLoading = true;
+
+            await api(true, true).get(endpointUrl)
+                .then((resp: UserResponseSuccess) => this.me = resp.data.data)
+
+            this.isLoading = false;
+        },
+    },
+});
