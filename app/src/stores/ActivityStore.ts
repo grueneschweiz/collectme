2d1d46970fe8f36347decbb47504751fa204c550
Cyrill Bolliger <mail@cyrill.me>
2022-07-07 21:03:31 +0200

    [FEAT] duplication safe activity update

--- app/src/stores/ActivityStore.ts
+++ app/src/stores/ActivityStore.ts
@@ -18,15 +18,52 @@ interface ActivityStoreState {
     activities: Activity[];
     isLoading: boolean;
     error: any | null;
+    prev: string | null;
     next: string | null;
     first: string | null;
 }
 
 function addResponseData(response: ActivityResponseSuccess) {
+    useActivityStore().prev = response.data.links.prev ?? null;
     useActivityStore().next = response.data.links.next ?? null;
     useActivityStore().first = response.data.links.first ?? null;
-    useActivityStore().activities.push(...response.data.data);
+    addActivities(response.data.data);
     useGroupStore().addGroups(response.data.included);
+}
+
+function addActivities(newActivities: Activity[]) {
+    const storedActivities: Activity[] = useActivityStore().activities;
+
+    if (!storedActivities.length) {
+        storedActivities.push(...newActivities);
+        return;
+    }
+
+    newActivities.reverse().forEach((newActivity: Activity) => {
+        if (!newActivity.attributes.created){
+            return;
+        }
+
+        if (newActivity.attributes.created > storedActivities[0]!.attributes.created!) {
+            storedActivities.unshift(newActivity);
+            return;
+        }
+
+        if (newActivity!.attributes.created < storedActivities[storedActivities.length - 1]!.attributes.created!) {
+            storedActivities.push(newActivity);
+            return;
+        }
+
+        for (let i = 0; i < storedActivities.length; i++) {
+            if (newActivity.attributes.created >= storedActivities[i].attributes.created!) {
+                if (newActivity.id !== storedActivities[i].id) {
+                    storedActivities.splice(i, 0, newActivity);
+                }
+
+                return;
+            }
+        }
+    })
 }
 
 export const useActivityStore = defineStore('ActivityStore', {
@@ -35,6 +72,7 @@ export const useActivityStore = defineStore('ActivityStore', {
             activities: [],
             isLoading: false,
             error: null,
+            prev: null,
             next: null,
             first: null,
         }
@@ -74,5 +112,24 @@ export const useActivityStore = defineStore('ActivityStore', {
                 this.isLoading = false;
             }
         },
+
+        async update() {
+            if (!this.prev) {
+                return this.fetchFirst();
+            }
+
+            this.isLoading = true;
+            this.error = null;
+
+            try {
+                await api(false, true)
+                    .get(this.prev)
+                    .then((resp: ActivityResponseSuccess) => addResponseData(resp))
+            } catch (error: any) {
+                this.error = error;
+            } finally {
+                this.isLoading = false;
+            }
+        }
     },
 });
