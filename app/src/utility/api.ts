71234ce8f514f18531dbf8a954238196a186046f
Cyrill Bolliger <mail@cyrill.me>
2022-07-08 22:31:17 +0200

    [FEAT] proper error handling for api

--- app/src/utility/api.ts
+++ app/src/utility/api.ts
@@ -1,16 +1,92 @@
-import axios from 'axios';
+import axios, {AxiosError} from 'axios';
+import axiosRetry from 'axios-retry';
+import router from "@/router";
+import type {Snackbar} from "@/stores/SnackbarStore";
+import {useSnackbarStore} from "@/stores/SnackbarStore";
+import t from '@/utility/i18n';
 
-export default () => {
-    return axios.create({
+
+function errorHandler(error: any) {
+    console.log(error);
+
+    if (axios.isAxiosError(error) && error.response.status === 401) {
+        useSnackbarStore().show({
+            id: error.response.status.toString(),
+            type: 'error',
+            shortDesc: t('General.Error.unauthenticated'),
+            vanishAfter: 5000,
+        } as Snackbar);
+
+        router.push('/login');
+        return Promise.reject(error);
+    }
+
+    if (!axios.isAxiosError(error)) {
+        useSnackbarStore().show({
+            id: error.toString(),
+            type: 'error',
+            shortDesc: t('General.Error.unspecificTitle'),
+            longDesc: t('General.Error.blameTheGoblins'),
+            vanishAfter: 10000,
+        } as Snackbar);
+        return Promise.reject(error);
+    }
+
+    const axiosError = (error as AxiosError);
+
+    if (axiosError.response?.status === 422) {
+        // 422 is a validation error
+        return Promise.reject(error);
+    }
+
+    let msg: Snackbar;
+    if (axiosError.response?.status === 500) {
+        if (axiosError.response.data) {
+            console.log(axiosError.response.data);
+        }
+
+        msg = {
+            id: axiosError.request.url,
+            type: 'error',
+            shortDesc: t('General.Error.blameTheDevTitle'),
+            longDesc: t('General.Error.blameTheDev',
+                {email: atob(collectme.encodedAdminEmail)}
+            ),
+            vanishAfter: 30000,
+        } as Snackbar
+    } else {
+        msg = {
+            id: axiosError.request.url,
+            type: 'error',
+            shortDesc: t('General.Error.unspecificTitle'),
+            longDesc: t('General.Error.blameTheGoblins'),
+            actionLabel: t('General.Error.tryAgain'),
+            action: () => useApi(true).request(axiosError.config),
+        } as Snackbar
+    }
+
+    useSnackbarStore().show(msg);
+
+    return Promise.reject(error);
+}
+
+export default function useApi(handleErrors: boolean = false) {
+    const instance = axios.create({
         baseURL: collectme.apiBaseUrl,
         headers: {
             'X-WP-Nonce': collectme.nonce,
         },
         xsrfHeaderName: 'X-WP-Nonce',
     });
-}
 
-export function handleUnexpectedError(error: any) {
-    console.log(error);
-    alert('Unexpected Error. Please try again later.');
+    axiosRetry(instance);
+
+    if (handleErrors) {
+        instance.interceptors.response.use(
+            (response: any) => response,
+            (error: any) => errorHandler(error),
+        );
+    }
+
+    return instance;
 }
