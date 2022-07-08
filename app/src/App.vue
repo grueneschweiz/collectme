f7c3937dec8a2e6b6da64f9fc91200516def1efe
Cyrill Bolliger <mail@cyrill.me>
2022-07-08 22:31:35 +0200

    [FEAT] add snackbar to app

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import TheSnackbar from "@/components/base/TheSnackbar/TheSnackbar.vue";
 </script>
 
 <template>
   <RouterView />
+  <TheSnackbar/>
 </template>
 
 <style>
