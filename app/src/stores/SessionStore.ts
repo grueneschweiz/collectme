f2dc210e8f052de2124f514234a23c30f1fdc473
Cyrill Bolliger <mail@cyrill.me>
2022-07-13 11:51:35 +0200

    [FEAT] session store

---
+++ app/src/stores/SessionStore.ts
@@ -0,0 +1,76 @@
+import type {AxiosResponse} from "axios";
+import type {Session} from "@/models/generated";
+import {defineStore} from "pinia";
+import type {AxiosError} from "axios";
+import api from "@/utility/api";
+import axios from "axios";
+import router from "@/router";
+import {useSnackbarStore} from "@/stores/SnackbarStore";
+import type {Snackbar} from "@/stores/SnackbarStore";
+import t from "@/utility/i18n";
+
+const endpointUrl = 'sessions/current';
+
+export interface SessionResponseSuccess extends AxiosResponse {
+    data: {
+        data: Session;
+    }
+}
+
+interface SessionStoreState {
+    session: Session | null;
+    isLoading: boolean;
+    error: AxiosError | null;
+    waitingForActivation: boolean;
+}
+
+export const useSessionStore = defineStore('SessionStore', {
+    state: (): SessionStoreState => {
+        return {
+            session: null,
+            isLoading: false,
+            error: null,
+            waitingForActivation: false,
+        }
+    },
+
+    actions: {
+        setSession(session: Session) {
+            this.session = session;
+        },
+
+        async fetch() {
+            this.isLoading = true;
+
+            try{
+                await api(false).get(endpointUrl)
+                    .then((resp: SessionResponseSuccess) => this.session = resp.data.data)
+            } catch (error: any) {
+                if (error && axios.isAxiosError(error)) {
+                    this.waitingForActivation = error.response?.status === 401;
+                }
+
+                if (error && axios.isAxiosError(error) && error.response?.status === 404) {
+                    await router.push('/login');
+                    return Promise.reject(error);
+                }
+
+                if (!this.waitingForActivation) {
+                    useSnackbarStore().show({
+                        id: error.toString(),
+                        type: 'error',
+                        shortDesc: t('General.Error.unspecificTitle'),
+                        longDesc: t('General.Error.blameTheGoblins'),
+                        vanishAfter: 10000,
+                    } as Snackbar);
+                }
+
+                return Promise.reject(error);
+            } finally {
+                this.isLoading = false;
+            }
+
+            return Promise.resolve();
+        }
+    }
+});
