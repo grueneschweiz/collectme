c9e33cbe86139acd43ca41c78bde54b6ef022321
Cyrill Bolliger <mail@cyrill.me>
2022-07-13 17:45:49 +0200

    [UX] improved success text

--- views/activation-success.php
+++ views/activation-success.php
@@ -1,5 +1,5 @@
 <div class="collectme-success">
     <h2><?php _e('Success','collectme') ?></h2>
-    <p><?php _e('You successfully activated your login. Head back to the browser on which you requested the login, you should now be automatically connected.', 'collectme') ?></p>
-    <a href="<?php echo get_permalink() ?>"><?php _e('Try again', 'collectme') ?></a>
+    <p><?php _e('You successfully activated your login. Head back to the browser on which you requested the login. You are now automatically signed in.', 'collectme') ?></p>
+    <a href="<?php echo get_permalink() ?>"><?php _e('Login here in this window', 'collectme') ?></a>
 </div>
