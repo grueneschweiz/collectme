1ad8ded50ecab921b708d3f688735d20c713d05e
Cyrill Bolliger <mail@cyrill.me>
2022-10-14 22:03:19 +0200

    [FEAT] WIP: MailScheduler

--- src/Misc/MailScheduler.php
+++ src/Misc/MailScheduler.php
@@ -5,31 +5,45 @@ declare(strict_types=1);
 namespace Collectme\Misc;
 
 
+use Collectme\Exceptions\CollectmeDBException;
 use Collectme\Model\Entities\Cause;
+use Collectme\Model\Entities\EnumGroupType;
 use Collectme\Model\Entities\Group;
+use DI\DependencyException;
+use DI\NotFoundException;
 
 class MailScheduler
 {
+    public function __construct(
+        private readonly Settings $settings,
+    ) {
+    }
+
     public static function scheduleCron(): void
     {
-        if ( ! wp_next_scheduled( 'collectme_schedule_mails' ) ) {
-            wp_schedule_event( time(), 'daily', 'collectme_schedule_mails' );
+        if (!wp_next_scheduled('collectme_schedule_mails')) {
+            wp_schedule_event(time(), 'daily', 'collectme_schedule_mails');
         }
     }
 
     public static function removeCron(): void
     {
-        $timestamp = wp_next_scheduled( 'collectme_schedule_mails' );
-        wp_unschedule_event( $timestamp, 'collectme_schedule_mails' );
+        $timestamp = wp_next_scheduled('collectme_schedule_mails');
+        wp_unschedule_event($timestamp, 'collectme_schedule_mails');
     }
 
+    /**
+     * @throws NotFoundException
+     * @throws CollectmeDBException
+     * @throws DependencyException
+     */
     public function run(): void
     {
         $container = collectme_get_container();
 
-        foreach(Cause::findAllActive() as $cause) {
-            foreach(Group::findPersonalByCause($cause->uuid) as $group) {
-                foreach($group->users() as $user) {
+        foreach ($this->getActiveCauses() as $cause) {
+            foreach (Group::findByTypeAndCause($cause->uuid, EnumGroupType::PERSON) as $group) {
+                foreach ($group->users() as $user) {
                     if (!$user->mailPermission) {
                         return;
                     }
@@ -39,5 +53,20 @@ class MailScheduler
                 }
             }
         }
+    }
+
+    /**
+     * @throws CollectmeDBException
+     */
+    public function getActiveCauses(): array
+    {
+        $now = date_create();
+
+        return array_filter(Cause::findAll(), function (Cause $cause) use ($now) {
+            [$start, $stop] = $this->settings->getTimings($cause->uuid);
+
+            return !($start && $start > $now)
+                && !($stop && $stop < $now);
+        });
     }
 }
